507da69bbe64e813c19e8b426afb50782d8d8029
SlimeVidas <dahan.isaac@gmail.com>
2021-02-18 20:16:19 +0200

    added-authorTypeToRootQuery

--- server/schema/Schema.js
+++ server/schema/Schema.js
@@ -8,7 +8,8 @@ const {
      GraphQLObjectType ,
      GraphQLString , 
      GraphQLSchema,
-     GraphQLID
+     GraphQLID,
+     GraphQLInt
     } = graphql;
 
 //dummy data
@@ -18,19 +19,29 @@ var books = [
     {name : "The Long Earth" , genre : "Sci-Fi" , id :"3"}
 ]
 
-var movies = [
-    {name : "Ben Hur" , genre : "History" , id :"1"},
-    {name : "Tequila Sunrise" , genre : "Romance" , id :"2"},
-    {name : "Orange Mecanique" , genre : "Sci-Fi" , id :"3"}
+var authors = [
+    {name : "Tom Wolf" , age : 45 , id :"1"},
+    {name : "Michel Stern" , age : 60 , id :"2"},
+    {name : "Guillaume Musso" , age : 58 , id :"3"}
 ]
 
 //create Book Type
 const BookType = new GraphQLObjectType({
     name : 'Book',
     fields : ()=>({
-        id : {type : graphql.GraphQLID},
+        id : {type : GraphQLID},
         name : {type : GraphQLString},
         genre : {type : GraphQLString}
+    })
+});
+
+//create Book Type
+const AuthorType = new GraphQLObjectType({
+    name : 'Author',
+    fields : ()=>({
+        id : {type : GraphQLID},
+        name : {type : GraphQLString},
+        age : {type :GraphQLInt}
     })
 });
 
@@ -42,10 +53,17 @@ const RootQuery = new GraphQLObjectType({
     fields : {
         book : {
             type : BookType , 
-            args : {id : {type : graphql.GraphQLID}},
+            args : {id : {type : GraphQLID}},
             resolve(parent , args){
               //code to get data from database
              return  _.find(books , {id : args.id})
+            }
+        },
+        author : {
+            type : AuthorType,
+            args : {id : {type : GraphQLID}},
+            resolve(parent , args){
+                return _.find(authors , {id : args.id})
             }
         }
     }
