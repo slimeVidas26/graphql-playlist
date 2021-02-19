02fc6f9c1ce49f4ab10ef63b3838db6284fbd101
SlimeVidas <dahan.isaac@gmail.com>
2021-02-19 12:54:34 +0200

    from-scratch-to-rootQuery

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ const schema = require('./schema/Schema')
 const app = express();
 
 app.use('/graphql' , graphqlHTTP({
-   schema,
-   graphiql : true
+schema,
+graphiql : true
 }))
 
 app.listen(4000 , ()=>{
-    console.log("Listen on port 4000")
+    console.log('Listen on port 4000')
 })
--- server/schema/Schema.js
+++ server/schema/Schema.js
@@ -1,75 +1,65 @@
-
-//require graphql
 const graphql = require('graphql');
-const _  = require('lodash')
-
-//destructuring graphql Object
-const {
-     GraphQLObjectType ,
-     GraphQLString , 
-     GraphQLSchema,
+const {GraphQLObjectType ,
+     GraphQLString ,
+     GraphQLInt,
      GraphQLID,
-     GraphQLInt
+     GraphQLSchema,
     } = graphql;
+const _ = require('lodash')
 
 //dummy data
 var books = [
-    {name : "Name of the Wind" , genre : "Fantasy" , id :"1"},
-    {name : "The Final Empire" , genre : "Fantasy" , id :"2"},
-    {name : "The Long Earth" , genre : "Sci-Fi" , id :"3"}
-]
+    {name : "Le nom de la rose" , genre : "History" , id : "1"},
+    {name : "Piege de Cristal" , genre : "Policier" , id : "2"},
+    {name : "5Th Avenue" , genre : "Sci-Fi" , id : "3"},
+];
 
 var authors = [
-    {name : "Tom Wolf" , age : 45 , id :"1"},
-    {name : "Michel Stern" , age : 60 , id :"2"},
-    {name : "Guillaume Musso" , age : 58 , id :"3"}
-]
+    {name : "John Le Carre" , age : 45 , id : "1"},
+    {name : "Guillaume Musso" , age : 56 , id : "2"},
+    {name : "Douglas Kennedy" , age : 68 , id : "3"},
+];
 
-//create Book Type
+ //create Book Type
 const BookType = new GraphQLObjectType({
-    name : 'Book',
-    fields : ()=>({
+    name  : 'Book',
+    fields : {
         id : {type : GraphQLID},
         name : {type : GraphQLString},
         genre : {type : GraphQLString}
-    })
+    }
 });
 
-//create Book Type
+//create Author Type
 const AuthorType = new GraphQLObjectType({
-    name : 'Author',
-    fields : ()=>({
+    name  : 'Author',
+    fields : {
         id : {type : GraphQLID},
         name : {type : GraphQLString},
-        age : {type :GraphQLInt}
-    })
+        age : {type : GraphQLInt}
+    }
 });
-
-
 
 //create Root Query
 const RootQuery = new GraphQLObjectType({
     name : 'RootQueryType',
     fields : {
-        book : {
-            type : BookType , 
-            args : {id : {type : GraphQLID}},
-            resolve(parent , args){
-              //code to get data from database
-             return  _.find(books , {id : args.id})
-            }
-        },
-        author : {
-            type : AuthorType,
-            args : {id : {type : GraphQLID}},
-            resolve(parent , args){
-                return _.find(authors , {id : args.id})
-            }
+       book : {
+           type : BookType,
+           args : {id:{type:GraphQLID}},
+           resolve(parent , args){
+              return _.find(books , {id : args.id})
+           }
+       },
+       author : {
+        type : AuthorType,
+        args : {id:{type:GraphQLID}},
+        resolve(parent , args){
+           return _.find(authors , {id : args.id})
         }
+    } 
     }
 });
-
-
 
 module.exports = new GraphQLSchema({
     query : RootQuery
