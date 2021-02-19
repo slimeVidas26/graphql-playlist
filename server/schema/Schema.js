13834881f32d27380b38611eadb76779c30939f0
SlimeVidas <dahan.isaac@gmail.com>
2021-02-19 13:58:48 +0200

    type-relations

--- server/schema/Schema.js
+++ server/schema/Schema.js
@@ -9,35 +9,42 @@ const _ = require('lodash')
 
 //dummy data
 var books = [
-    {name : "Le nom de la rose" , genre : "History" , id : "1"},
-    {name : "Piege de Cristal" , genre : "Policier" , id : "2"},
-    {name : "5Th Avenue" , genre : "Sci-Fi" , id : "3"},
+    {name : "Le nom de la rose" , genre : "History" , id : "1" , authorId:1},
+    {name : "Piege de Cristal" , genre : "Policier" , id : "2" , authorId:3},
+    {name : "5Th Avenue" , genre : "Sci-Fi" , id : "3" , authorId:2},
 ];
 
 var authors = [
-    {name : "John Le Carre" , age : 45 , id : "1"},
-    {name : "Guillaume Musso" , age : 56 , id : "2"},
-    {name : "Douglas Kennedy" , age : 68 , id : "3"},
+    {name : "John Le Carre" , age : 45 , id : 1},
+    {name : "Guillaume Musso" , age : 56 , id : 2},
+    {name : "Douglas Kennedy" , age : 68 , id : 3},
 ];
 
  //create Book Type
 const BookType = new GraphQLObjectType({
     name  : 'Book',
-    fields : {
+    fields :()=> ({
         id : {type : GraphQLID},
         name : {type : GraphQLString},
-        genre : {type : GraphQLString}
-    }
+        genre : {type : GraphQLString},
+        author : {
+            type : AuthorType,
+           resolve(parent , args){
+               console.log(parent);
+               return  _.find(authors , {id : parent.authorId})
+           }
+        }
+    })
 });
 
 //create Author Type
 const AuthorType = new GraphQLObjectType({
     name  : 'Author',
-    fields : {
+    fields :()=> ({
         id : {type : GraphQLID},
         name : {type : GraphQLString},
         age : {type : GraphQLInt}
-    }
+    })
 });
 
 //create Root Query
