fe72f380ef30e5714711258e81e388eb4ab0bf1a
SlimeVidas <dahan.isaac@gmail.com>
2021-02-22 20:03:51 +0200

    create-react-app-and-Booklist-component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookList from './components/BookList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book List</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function BookList() {
+    return (
+      <div className="book-list">
+        <ul>
+            <li>First book</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default BookList;
